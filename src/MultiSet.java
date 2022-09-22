28a91917e0b4fed58a259375d8195683afb012fd
Eric R <ryttingeric@gmail.com>
2022-09-22 15:27:22 -0600

    Initial commit

---
+++ src/MultiSet.java
@@ -0,0 +1,25 @@
+/**
+ * Sometimes the number of times an element occurs In an unordered collection matters.
+ * A multiset (or mset or bag) is an unordered collection of elements where an
+ * element can occur as a member more than once
+ * (see https://en.wikipedia.org/wiki/Multiset).
+ * Given multisets A and B from the same universal set, find
+ * A∪B, A∩B, A – B, and A + B.
+ */
+public class MultiSet {
+    public boolean[] orMultiSet(boolean[] setA, boolean[] setB){
+        return null;//TODO
+    }
+
+    public boolean[] andMultiSet(boolean[] setA, boolean[] setB){
+        return null; //TODO
+    }
+
+    public boolean[] subtractMultiSet(boolean[] setA, boolean[] setB){
+        return null; //TODO
+    }
+
+    public boolean[] addMultiSet(boolean[] setA, boolean[] setB){
+        return null; //TODO
+    }
+}
---
+++ src/Set.java
@@ -0,0 +1,29 @@
+/**
+ * Given subsets A and B of a set with n elements,
+ * use bit strings or an array of Booleans to find
+ * not(A), A∪B, A∩B, A – B, and A ⊕ B
+ * (the symmetric difference of A and B defined as (A – B) ∪ (B – A)
+ */
+public class Set {
+    public boolean[] notSet(boolean[] set){
+        return null;//TODO
+    }
+
+    public boolean[] orSets(boolean[] setA, boolean[] setB){
+        return null;//TODO
+    }
+
+    public boolean[] andSets(boolean[] setA, boolean[] setB){
+        return null; //TODO
+    }
+
+    public boolean[] subtractSets(boolean[] setA, boolean[] setB){
+        return null; //TODO
+    }
+
+    public boolean[] xorSets(boolean[] setA, boolean[] setB){
+        return null; //TODO
+    }
+}
+
+
